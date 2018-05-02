af715d216e8e42c45a40c8961a54ddf4d42cfbd5
johnhckuo <johnhckuo@gmail.com>
2018-05-03 00:06:37 +0800

    reply handler added

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => {
-      replyMessage(result);
-    })
+  if (!Array.isArray(req.body.events)) {
+    return res.status(500).end();
+  }
+
+  // handle events separately
+  Promise.all(req.body.events.map(handleEvent))
+    .then(() => res.end())
     .catch((err) => {
       console.error(err);
       res.status(500).end();
@@ -33,17 +35,127 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          return handleText(message, event.replyToken, event.source);
+        case 'sticker':
+          return handleSticker(message, event.replyToken);
+        case 'location':
+          return handleLocation(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case 'follow':
+      return replyText(event.replyToken, 'Got followed event');
+
+    case 'unfollow':
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case 'join':
+      return replyText(event.replyToken, `Joined ${event.source.type}`);
+
+    case 'leave':
+      return console.log(`Left: ${JSON.stringify(event)}`);
+
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
   }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
 }
+
+function handleText(message, replyToken, source) {
+
+  switch (message.text) {
+    case 'profile':
+      if (source.userId) {
+        return client.getProfile(source.userId)
+          .then((profile) => replyText(
+            replyToken,
+            [
+              `Display name: ${profile.displayName}`,
+              `Status message: ${profile.statusMessage}`,
+            ]
+          ));
+      } else {
+        return replyText(replyToken, 'Bot can\'t use profile API without user ID');
+      };
+    case 'confirm':
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'template',
+          altText: 'Confirm alt text',
+          template: {
+            type: 'confirm',
+            text: 'Do it?',
+            actions: [
+              { label: 'Yes', type: 'message', text: 'Yes!' },
+              { label: 'No', type: 'message', text: 'No!' },
+            ],
+          },
+        }
+      );
+    case 'datetime':
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'template',
+          altText: 'Datetime pickers alt text',
+          template: {
+            type: 'buttons',
+            text: 'Select date / time !',
+            actions: [
+              { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' },
+              { type: 'datetimepicker', label: 'time', data: 'TIME', mode: 'time' },
+              { type: 'datetimepicker', label: 'datetime', data: 'DATETIME', mode: 'datetime' },
+            ],
+          },
+        }
+      );
+    case 'bye':
+      switch (source.type) {
+        case 'user':
+          return replyText(replyToken, 'Bot can\'t leave from 1:1 chat');
+        case 'group':
+          return replyText(replyToken, 'Leaving group')
+            .then(() => client.leaveGroup(source.groupId));
+        case 'room':
+          return replyText(replyToken, 'Leaving room')
+            .then(() => client.leaveRoom(source.roomId));
+      }
+    default:
+      console.log(`Echo message to ${replyToken}: ${message.text}`);
+      return replyText(replyToken, message.text);
+  }
+}
+
+function handleSticker(message, replyToken) {
+  return client.replyMessage(
+    replyToken,
+    {
+      type: 'sticker',
+      packageId: message.packageId,
+      stickerId: message.stickerId,
+    }
+  );
+}
+
+function handleLocation(message, replyToken) {
+  return client.replyMessage(
+    replyToken,
+    {
+      type: 'location',
+      title: message.title,
+      address: message.address,
+      latitude: message.latitude,
+      longitude: message.longitude,
+    }
+  );
+}
+
 
 // listen on port
 const port = process.env.PORT || 8080;
@@ -51,36 +163,30 @@ app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
-// curl -v -X POST  \
-// -H 'Content-Type:application/json' \
-// -H 'Authorization: Bearer {channel access token}' \
-// -d ''
-// reply messages
 
-
-function replyMessage(replyToken){
-  axios({
-    method: 'post',
-    url: 'https://api.line.me/v2/bot/message/reply',
-    headers: {'content-type': "application/json", "Authorization": `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`},
-    data: {
-        "replyToken":replyToken,
-        "messages":[
-            {
-                "type":"text",
-                "text":"Hello, user"
-            },
-            {
-                "type":"text",
-                "text":"May I help you?"
-            }
-        ]
-    }
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-  });
-}
+// function replyMessage(replyToken){
+//   axios({
+//     method: 'post',
+//     url: 'https://api.line.me/v2/bot/message/reply',
+//     headers: {'content-type': "application/json", "Authorization": `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`},
+//     data: {
+//         "replyToken":replyToken,
+//         "messages":[
+//             {
+//                 "type":"text",
+//                 "text":"Hello, user"
+//             },
+//             {
+//                 "type":"text",
+//                 "text":"May I help you?"
+//             }
+//         ]
+//     }
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//   });
+// }
