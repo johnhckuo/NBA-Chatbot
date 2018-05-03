a83b6be6fdb94d0226a6ac432f0ba44d398f0843
Calhoun5 <johnhckuo@gmail.com>
2018-05-03 14:53:50 +0800

    testing ngrok

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const line = require('@line/bot-sdk');
 const express = require('express');
 const axios = require('axios');
-const handleEvent = require("./handler")
 
 // create LINE SDK config from env variables
 const config = {
@@ -17,6 +16,14 @@ const client = new line.Client(config);
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+app.get('/callback', function(req, res) {
+  res.send('hello callback');
+});
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
@@ -39,3 +46,138 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+
+
+
+// event handler
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          return handleText(message, event.replyToken, event.source);
+        case 'sticker':
+          return handleSticker(message, event.replyToken);
+        case 'location':
+          return handleLocation(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case 'follow':
+      return replyText(event.replyToken, 'Got followed event');
+
+    case 'unfollow':
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case 'join':
+      return replyText(event.replyToken, `Joined ${event.source.type}`);
+
+    case 'leave':
+      return console.log(`Left: ${JSON.stringify(event)}`);
+
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+}
+
+// simple reply function
+const replyText = (token, texts) => {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return client.replyMessage(
+    token,
+    texts.map((text) => ({ type: 'text', text }))
+  );
+};
+
+function handleText(message, replyToken, source) {
+
+  switch (message.text) {
+    case 'profile':
+      if (source.userId) {
+        return client.getProfile(source.userId)
+          .then((profile) => replyText(
+            replyToken,
+            [
+              `Display name: ${profile.displayName}`,
+              `Status message: ${profile.statusMessage}`,
+            ]
+          ));
+      } else {
+        return replyText(replyToken, 'Bot can\'t use profile API without user ID');
+      };
+    case 'confirm':
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'template',
+          altText: 'Confirm alt text',
+          template: {
+            type: 'confirm',
+            text: 'Do it?',
+            actions: [
+              { label: 'Yes', type: 'message', text: 'Yes!' },
+              { label: 'No', type: 'message', text: 'No!' },
+            ],
+          },
+        }
+      );
+    case 'datetime':
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'template',
+          altText: 'Datetime pickers alt text',
+          template: {
+            type: 'buttons',
+            text: 'Select date / time !',
+            actions: [
+              { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' },
+              { type: 'datetimepicker', label: 'time', data: 'TIME', mode: 'time' },
+              { type: 'datetimepicker', label: 'datetime', data: 'DATETIME', mode: 'datetime' },
+            ],
+          },
+        }
+      );
+    case 'bye':
+      switch (source.type) {
+        case 'user':
+          return replyText(replyToken, 'Bot can\'t leave from 1:1 chat');
+        case 'group':
+          return replyText(replyToken, 'Leaving group')
+            .then(() => client.leaveGroup(source.groupId));
+        case 'room':
+          return replyText(replyToken, 'Leaving room')
+            .then(() => client.leaveRoom(source.roomId));
+      }
+    default:
+      console.log(`Echo message to ${replyToken}: ${message.text}`);
+      return replyText(replyToken, message.text);
+  }
+}
+
+function handleSticker(message, replyToken) {
+  return client.replyMessage(
+    replyToken,
+    {
+      type: 'sticker',
+      packageId: message.packageId,
+      stickerId: message.stickerId,
+    }
+  );
+}
+
+function handleLocation(message, replyToken) {
+  return client.replyMessage(
+    replyToken,
+    {
+      type: 'location',
+      title: message.title,
+      address: message.address,
+      latitude: message.latitude,
+      longitude: message.longitude,
+    }
+  );
+}
